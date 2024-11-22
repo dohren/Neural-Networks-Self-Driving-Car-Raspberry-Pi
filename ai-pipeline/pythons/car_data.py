9c062dc825747befa09ad811cfe713aede474ef9
Smug Pepe <59608685+MovEaxEax@users.noreply.github.com>
2024-11-22 16:34:52 +0100

    ref: car_data.py
    Funktionen um Bild zu laden und zu trainings input zu converten hinzugefügt

--- ai-pipeline/pythons/car_data.py
+++ ai-pipeline/pythons/car_data.py
@@ -1,11 +1,44 @@
 import numpy as np
 import random
+import cv2
 
 
 
-# Tensorflow GPU setup
+# Load image from file
+def load_image(image_path):
+    image = cv2.imread(image_path)
+    if image is None:
+        raise FileNotFoundError(f"No image found at: '{image_path}'")
+    height, width = image.shape[:2]
+    return image, width, height
+
+# Convert image to black and white only (With threshold)
+def process_image(image_data):
+    gray = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
+    _, processed_black_white = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+    return processed_black_white
+
+# Convert processed image to 1 (white pxiel) and 0 (black pixel)
+def convert_image(image_processed):
+    int_array = (image_processed == 255).astype(int)
+    return int_array
+
+# Int array to NP array
 def binary_image_to_np_array(data):
     return np.array(data, dtype=np.float16)
+
+# Image from path to np array
+def image_path_to_np_array(image_path):
+    image_data = load_image(image_path)
+    image_processed = process_image(image_data)
+    image_int_array = convert_image(image_processed)
+    return binary_image_to_np_array(image_int_array)
+
+# Image from data to np array
+def image_data_to_np_array(image_data):
+    image_processed = process_image(image_data)
+    image_int_array = convert_image(image_processed)
+    return binary_image_to_np_array(image_int_array)
 
 # Create test dataset
 def binary_image_test_dataset(samples=100, input_dim=(64, 64), output_dim=2):
